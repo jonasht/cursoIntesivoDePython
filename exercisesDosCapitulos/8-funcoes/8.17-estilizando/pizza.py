8a75ab6a272414ac9e1f83fd09bd291742df106a
jonasht <jhenriquet@outlook.com.br>
2020-12-27 17:04:37 -0300

    arrumando

--- exercisesDosCapitulos/8-funcoes/8.17-estilizando/pizza.py
+++ exercisesDosCapitulos/8-funcoes/8.17-estilizando/pizza.py
@@ -1,7 +1,10 @@
 def fazer_pizza(tamanho, *ingredientes):
+    
     print()
+    
     print('tamanho ' + str(tamanho) + 
           ' da pizza com os ingredientes: ')
+    
     for ingrediente in ingredientes:
         print('-> ' + ingrediente.title())
     
