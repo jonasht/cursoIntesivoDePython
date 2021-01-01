87f5133a6be6613f75191aaddd330f6a15ed6252
jonasht <jhenriquet@outlook.com.br>
2021-01-01 01:54:21 -0300

    add exer terminado

---
+++ exercisesDosCapitulos/9-classes/9.8-privilegios/privilegios.py
@@ -0,0 +1,61 @@
+
+class User(): 
+    
+    def __init__(self, first_name, last_name, email, username, password):
+        # cadastrando usuario
+        self.first_name = first_name.title()
+        self.last_name = last_name.title()
+        self.email = email
+        self.username = username
+        self.password = password
+        
+        
+    def describe_user(self): # descrever usuario
+        print(f'     nome: {self.first_name}')
+        print(f'sobrenome: {self.last_name}')
+        print(f'    email: {self.email}')
+        print(f' username: {self.username}')
+        print(f' password: {self.password}')
+        
+    def greet(self):
+        print(f'saudacoes {self.first_name} {self.last_name}')
+
+
+class Admin(User):
+    def __init__(self, first_name, last_name, email, username, password):
+        super().__init__(first_name, last_name, email, username, password)
+        self.privilages = Privileges()
+        
+
+
+class Privileges():
+    def __init__(self, privilages=[]):
+        self.privilages = privilages
+        
+    def show_privilages(self):
+        print(f'\n privilegios:')
+        if self.privilages:
+            for privilage in self.privilages:
+                print(f'    -> {privilage}')
+        else:
+            print(f'o usuario nao tem')
+        print()
+    
+            
+        
+u1 = User('felipe', 'arcades', 'feliades@outlook.com', 'felip123', '123')
+
+u1.describe_user()
+u1.greet()
+
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+admin = Admin('jonas', 'jon', 'jonas@outlook.com', 'jonas123', '123')
+admin.describe_user()
+admin.privilages.privilages = [
+                    'pode adicionar post',
+                    'pode deletar post',
+                    'pode banir usuario'
+                    ]
+
+admin.privilages.show_privilages()
