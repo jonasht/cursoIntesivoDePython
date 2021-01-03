704f78455aaebd86c4d04828b3ba2936fc8579b7
jonasht <jhenriquet@outlook.com.br>
2021-01-03 02:13:44 -0300

    adicionando code e arrumando

---
+++ 9-classes/3-car/car-4/my_cars.py
@@ -0,0 +1,15 @@
+from car import Car
+from eletric_car import EletricCar
+
+print()
+
+meu_carro = Car('volkswagen', 'beetle', 2016)
+print(meu_carro.get_descriptive_name())
+
+meu_tesla = EletricCar('tesla', 'modelo s', 2016)
+print(meu_tesla.get_descriptive_name())
+
+
+
+
+print()
