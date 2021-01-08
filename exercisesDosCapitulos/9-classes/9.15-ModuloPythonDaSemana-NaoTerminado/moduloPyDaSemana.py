48877847802b9c1da478ee582e485956e2719a8b
jonasht <jhenriquet@outlook.com.br>
2021-01-07 21:50:21 -0300

    add

---
+++ exercisesDosCapitulos/9-classes/9.15-ModuloPythonDaSemana-NaoTerminado/moduloPyDaSemana.py
@@ -0,0 +1,11 @@
+
+#string_capwords.py
+'''visto no site https://pymotw.com/3/ '''
+
+import string
+
+print()
+s = f'a rapida raposa marrom pulou pelo cachorro preguiçoso'
+print(s)
+print(string.capwords(s))
+print()
