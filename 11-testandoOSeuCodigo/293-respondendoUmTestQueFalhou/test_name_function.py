7a381fe461fa57235f507ceaa20edec633d66325
jonasht <jhenriquet@outlook.com.br>
2021-04-14 09:31:26 -0300

    arrumando e adicionando

---
+++ 11-testandoOSeuCodigo/293-respondendoUmTestQueFalhou/test_name_function.py
@@ -0,0 +1,16 @@
+
+from name_fuction import get_formatted_name
+import unittest
+
+class namesTestCase(unittest.TestCase):
+    
+    def test_first_last_name(self):
+        formatted_name = get_formatted_name('jonas', 'teixeira')
+        self.assertEqual(formatted_name, 'Jonas Teixeira')
+
+    def test_first_last_middle_name(self):
+        formatted_name = get_formatted_name('jonas', 'teixeira', 'h')
+        self.assertEqual(formatted_name, 'Jonas H Teixeira')
+        
+unittest.main()
+
