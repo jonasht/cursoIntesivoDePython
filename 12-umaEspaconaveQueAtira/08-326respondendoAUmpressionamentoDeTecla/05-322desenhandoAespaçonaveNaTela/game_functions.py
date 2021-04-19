46f782768c7a96de01c6788a6e6b7a67fa30569e
jonasht <jhenriquet@outlook.com.br>
2021-04-19 08:47:17 -0300

    arrumando capitulos add novo exemplo cap 12

---
+++ 12-umaEspaconaveQueAtira/08-326respondendoAUmpressionamentoDeTecla/05-322desenhandoAespaçonaveNaTela/game_functions.py
@@ -0,0 +1,23 @@
+import sys
+
+import pygame
+
+def check_events(ship):
+    # responder events/eventos quando eh pressionado as teclas e o mouse
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                # mover nave para a direita
+                ship.rect.centerx += 1
+                
+def update_screen(ai_settings, screen, ship):
+    """atualiza as imagens na tela para nova tela"""        
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+
+    # deixar a tela mais receente visivel 
+    pygame.display.flip()
+    
+    
